a438a9a9998e0953cd2c0bb4ee534e3372ccce0c
Udaysaptale07 <saptaleuday2003@gmail.com>
2025-08-07 23:32:33 +0530

    Update uppercase.c

--- FBS/Assignment_1/uppercase.c
+++ FBS/Assignment_1/uppercase.c
@@ -3,7 +3,7 @@ int main()
 char alph;
 {
 	printf("Enter a alphabet");
-	scanf("%ch",&alph);
+	scanf("%c",&alph);
 	
 	if (alph>='A' || alph<='Z')
 	printf("Its uppercase");
@@ -14,6 +14,7 @@ char alph;
 		return 0;
 
 }
+
 
 
 
